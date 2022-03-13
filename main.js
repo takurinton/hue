9271848930c36024face14a735df15a41efdeb59
takurinton <takurinton@takurinton.dev>
2022-03-13 11:41:50 +0900

    add: randomLight

--- main.js
+++ main.js
@@ -6,9 +6,35 @@ const HUE_USERNAME = process.env.HUE_USERNAME;
 (async function () {
   const hue = await getHueLights();
   const lights = await hue.json();
-  console.log(lights);
+  
+  for (const light of Object.keys(lights)) {
+    randomLight(light);
+  }
 })();
 
 async function getHueLights () {
  return await fetch(`${HUE_ENDPOINT}/api/${HUE_USERNAME}/lights`);
+}
+
+const getRandomHue = () => Math.floor(Math.random() * 65535);
+
+async function randomLight (lightNumber) {
+  while (true) {
+    await fetch(`${HUE_ENDPOINT}/api/${HUE_USERNAME}/lights/${lightNumber}/state`, {
+      method: 'PUT',
+      body: JSON.stringify({
+         on: true,
+         bri: 200,
+         sat: 200,
+         hue: getRandomHue(),
+      }),
+    });
+  }
+}
+
+async function resetLight () {
+  async fetch(`${HUE_ENDPOINT}/api/${HUE_USERNAME}/lights/${lightNumber}/state`, {
+    method: 'PUT',
+    body: JSON.stringify({ on: false }),
+  })
 }
